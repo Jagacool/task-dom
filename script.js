233f2629fa805d090f816b96071acc32cd14d659
j <137029997+Jagacool@users.noreply.github.com>
2023-07-15 18:37:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ let wrapper = document.querySelector(".wrapper");
 const req = new XMLHttpRequest();
 req.open(
   "GET",
-  "https://gist.githubusercontent.com/rvsp/add40254aa126f045837fa5b51f47f1f/raw/4d724bfabf4cce7379a386e23bef6576ab99a2f9/pagination.json"
+  "https://gist.githubusercontent.com/Jagacool/d651890d00c1fc383501d6883199e664/raw/8d86b0c77b62573cb95befd87cc9c368f3955098/pagination.json"
 );
 req.send();
 req.addEventListener("load", request);
